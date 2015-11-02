4ca4e15479ab3d61ffb6fca341697ae75069681d
Cody Hatch <cody@chaos.net.nz>
2015-11-02 16:31:44 +1300

    Add date type
    Date type expects a date object or a string (which it tries to parse as a date).

--- src/typed.js
+++ src/typed.js
@@ -140,6 +140,14 @@ Typed.Boolean = Typed("Boolean", value =>
   value === false ? false :
   TypeError(`"${value}" is not a boolean`))
 
+Typed.Date = Typed("Date", value => {
+  if (Object.prototype.toString.call(value) === "[object Date]") {
+    return value
+  } else if (typeof(value) === "string") {
+    return new Date(value)
+  }
+  return TypeError(`"${value}" is not a date or string`)})
+
 class MaybeType extends Type {
   constructor(type) {
     super()
